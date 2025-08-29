802aa9e8721a9b4986fe24b44f7914389e96c251
Eric Sun <eric.sun@plu.edu>
2025-08-28 23:51:26 -0700

    attempt at building model (something about mismatching mat1 and mat2 dimensions)

---
+++ pytorch_stuff/analysis_shattered.py
@@ -0,0 +1,61 @@
+import os
+import json
+from itertools import chain
+
+
+path = "./data"
+mob_ids = json.load(open("mob_names.json"))
+
+def progression_score(state):
+    max_depth = int(state["game_bundle"]["maxDepth"])
+    hero_level = int(state["game_bundle"]["hero"]["lvl"])
+
+    return min((max_depth*hero_level*65), 50000)
+
+def gen_lines(data_path: str):
+    for file in os.listdir(data_path): 
+        if file.endswith(".jsonl"):
+            for line in open(f"{data_path}/{file}"):
+                yield json.loads(line)
+
+# all_mobs = chain.from_iterable(line["state"]["level_bundle"]["level"]["mobs"] for line in gen_transitions(path))
+# mob_names = {mob["__className"] for mob in all_mobs}
+
+# for i, name in enumerate(mob_names):
+#     print(name, i)
+
+def state_to_junk(data: dict[str, any]):
+    map = data["level_bundle"]["level"]["map"]
+    level_width = data["level_bundle"]["level"]["width"]
+    hero_pos = data["game_bundle"]["hero"]["pos"]
+    mobs = data["level_bundle"]["level"]["mobs"]
+
+    projected_coords = list(gen_indexes(hero_pos, level_width))
+
+    projected_map = {x: map[x] if 0 <= x < len(map) else None for x in projected_coords}
+
+    mob_data = {m["pos"]: [m["HP"], mob_ids.get(m["__className"])] for m in mobs if m["seen"] and m["pos"] in projected_coords}
+
+    tensor_data = chain.from_iterable([[projected_map.get(x, None)] + mob_data.get(x, [None, None]) for x in projected_coords])
+    return tensor_data
+
+
+def gen_indexes(pos, width, proj_size=20):
+    pos_x, pos_y = gen_coords(pos, width)
+    for dx in range(-proj_size//2, proj_size//2):
+        for dy in range(-proj_size//2, proj_size//2):
+            yield ungen_coords(pos_x + dx, pos_y + dy, width)
+
+def gen_coords(pos, width):
+    x = pos % width
+    y = pos // width
+    return (x, y)
+
+def ungen_coords(x, y, width):
+    pos = x + y * width
+    return pos
+
+def gen_transitions(data_path):
+    for line in gen_lines(data_path):
+        yield (list(state_to_junk(line["state"])), line["action"], list(state_to_junk(line["next_state"])), progression_score(line["next_state"]))
+        
---
+++ pytorch_stuff/train.py
@@ -0,0 +1,159 @@
+from collections import namedtuple
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import math
+import random
+from analysis_shattered import gen_transitions
+import itertools
+
+
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
+
+
+print("device", device)
+
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+
+# BATCH_SIZE is the number of transitions sampled from the replay buffer
+# GAMMA is the discount factor as mentioned in the previous section
+# EPS_START is the starting value of epsilon
+# EPS_END is the final value of epsilon
+# EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+# TAU is the update rate of the target network
+# LR is the learning rate of the ``AdamW`` optimizer
+
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.01
+EPS_DECAY = 2500
+TAU = 0.005
+LR = 3e-4
+
+
+# Get number of actions from gym action space
+n_actions = 9
+# TODO populate with our data (from analysis file)
+path = "./data"
+
+print("building transitions...")
+transitions : list[Transition] = [Transition(
+    torch.tensor([s or 0 for s in state], dtype=torch.float), 
+    torch.tensor([action], dtype=torch.int), 
+    torch.tensor([s or 0 for s in next_state], dtype=torch.float), 
+    torch.tensor([reward], dtype=torch.float)
+    ) for state, action, next_state, reward in itertools.islice(gen_transitions(path), 1000)]
+
+# TODO make some replacement for env
+# Get the number of state observations
+# state, info = env.reset()
+n_observations = len(transitions[0].state) # total number of observations that we have
+print("n_observations", n_observations)
+
+policy_net = DQN(n_observations, n_actions).to(device)
+target_net = DQN(n_observations, n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+
+
+steps_done = 0
+
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return the largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policy_net(state).max(1).indices.view(1, 1)
+    else:
+        return torch.tensor([[random.randint(1, 9)]], device=device, dtype=torch.long)
+
+
+def optimize_model():
+    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation). This converts batch-array of Transitions
+    # to Transition of batch-arrays.
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
+    state_batch = torch.stack([t.state for t in transitions])
+    action_batch = torch.stack([t.action for t in transitions])
+    reward_batch = torch.stack([t.reward for t in transitions])
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+    input_tensor = policy_net(state_batch)
+    state_action_values = input_tensor.gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1).values
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    with torch.no_grad():
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1).values
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    # In-place gradient clipping
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step()
+
+
+def save_model(model_file: str):
+    torch.save(policy_net.state_dict(), model_file)
+
+
+def train_model_mass_data():
+    print("optimizing model...")
+    optimize_model()
+    print("saving model...")
+    save_model("first_model.pt")
+
+train_model_mass_data()
